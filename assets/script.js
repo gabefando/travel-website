01400aa20be8946eca9696ad391783bd35e8824d
Tanner Shadday <tshadday99@gmail.com>
2022-07-06 13:58:57 -0400

    added working hotel info fetches

--- assets/script.js
+++ assets/script.js
@@ -13,10 +13,32 @@ function inputSearch() {
 	if (localStorage.getItem("search")) {
         let x = localStorage.getItem("search");
         input.innerText = x;
+		
 		fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query='+x+'&locale=en_US&currency=USD', options)
 		.then(response => response.json())
-		.then(response => console.log(response))
-		.catch(err => console.error(err));
+		.then(function(response){
+				let entities = response.suggestions[1].entities;
+				console.log(entities);
+				for(let i = 0; i < entities.length; i++) {
+					var destId = entities[i].destinationId;
+					function fetchDetails() {
+						fetch('https://hotels4.p.rapidapi.com/properties/get-details?id='+destId+'', options)
+						.then(response => response.json())
+						.then(function(response){
+							console.log(response);
+							var hotelId = response.data.body.pdpHeader.hotelId;
+							function fetchImages() {
+								fetch('https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id='+hotelId+'', options)
+								.then(response => response.json())
+								.then(response => console.log(response))
+								.catch(err => console.error(err));
+							};
+						fetchImages();
+						})
+					};
+				fetchDetails();
+				};
+			});		
     }
 };
 
