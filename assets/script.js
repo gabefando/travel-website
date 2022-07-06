58e1200a365aaaa42c3628a794d839b57db3990c
Tanner Shadday <tshadday99@gmail.com>
2022-07-06 11:37:12 -0400

    added working local storage and travel API

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var input = document.querySelector("#input").value;
+var input = document.getElementById("input");
 
 // Hotels API
 const options = {
@@ -9,11 +9,22 @@ const options = {
 	}
 };
 
-fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query='+input+'&locale=en_US&currency=USD', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-function() {
-    // use add event listener so that when button is clicked, the js is updated
+function inputSearch() {
+	if (localStorage.getItem("search")) {
+        let x = localStorage.getItem("search");
+        input.innerText = x;
+		fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query='+x+'&locale=en_US&currency=USD', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+    }
 };
+
+// use add event listener so that when button is clicked, the js is updated
+document.getElementById("btn").addEventListener("click", function(event) {
+	event.preventDefault;
+	localStorage.setItem("search", input.value);
+	inputSearch();
+});
+
+inputSearch();
